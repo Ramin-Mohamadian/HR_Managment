41e8a24e559c976481ba7a7575d137a1a3cc7824
Ramin-Mohamadian <raminmohamadian2020@gmail.com>
2024-08-20 17:35:18 +0330

    Write Generic Reosotory

---
+++ HR_Management.Application/Persistence/Contracts/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using HR_Management.Domain.Entiteis;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_Management.Application.Persistence.Contracts
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> Get(int id);
+        Task<IEnumerable<T>> GetAll();
+        Task<T> Add(T entity);
+        Task<T> Update(T entity);
+        Task<T> Delete(T entity);
+
+    }
+}
