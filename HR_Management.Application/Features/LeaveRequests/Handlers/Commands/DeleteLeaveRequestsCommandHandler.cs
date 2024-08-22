ac1ccfefed49ceb8ef28c5921fa40d0b01b85d59
Ramin-Mohamadian <raminmohamadian2020@gmail.com>
2024-08-22 13:02:43 +0330

    Write Delete Handler

---
+++ HR_Management.Application/Features/LeaveRequests/Handlers/Commands/DeleteLeaveRequestsCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using HR_Management.Application.Features.LeaveRequests.Requests.Commands;
+using HR_Management.Application.Persistence.Contracts;
+using MediatR;
+
+namespace HR_Management.Application.Features.LeaveRequests.Handlers.Commands
+{
+    public class DeleteLeaveRequestsCommandHandler : IRequestHandler<DeleteLeaveRequestsCommand, Unit>
+    {
+        private readonly ILeaveRequestRepository _leaveRequestRepository;
+        private readonly IMapper _mapper;
+
+        public DeleteLeaveRequestsCommandHandler(ILeaveRequestRepository leaveRequestRepository, IMapper mapper)
+        {
+            _leaveRequestRepository = leaveRequestRepository;
+            _mapper = mapper;
+        }
+        public async Task<Unit> Handle(DeleteLeaveRequestsCommand request, CancellationToken cancellationToken)
+        {
+            var deleteLeaveRequest=await _leaveRequestRepository.Get(request.Id);
+            await _leaveRequestRepository.Delete(deleteLeaveRequest);
+            return Unit.Value;
+        }
+    }
+}
---
+++ HR_Management.Application/Features/LeaveTypes/Handlers/Commands/DeleteLeaveTypesCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using HR_Management.Application.Features.LeaveTypes.Requests.Commands;
+using HR_Management.Application.Persistence.Contracts;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_Management.Application.Features.LeaveTypes.Handlers.Commands
+{
+    public class DeleteLeaveTypesCommandHandler : IRequestHandler<DeleteLeaveTypesCommand, Unit>
+    {
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+        private readonly IMapper _mapper;
+
+        public DeleteLeaveTypesCommandHandler(ILeaveTypeRepository leaveTypeRepository,IMapper mapper)
+        {
+            _leaveTypeRepository = leaveTypeRepository;
+            _mapper = mapper;
+        }
+        public async Task<Unit> Handle(DeleteLeaveTypesCommand request, CancellationToken cancellationToken)
+        {
+            var deleteLeavetype = await _leaveTypeRepository.Get(request.Id);
+            await _leaveTypeRepository.Delete(deleteLeavetype);
+            return Unit.Value;
+        }
+    }
+}
