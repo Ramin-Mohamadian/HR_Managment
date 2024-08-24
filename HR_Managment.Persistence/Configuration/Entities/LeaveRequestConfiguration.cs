f7e56fe98429f3a0c0b2a2e8b2767d043e51ebf5
Ramin-Mohamadian <raminmohamadian2020@gmail.com>
2024-08-24 23:02:44 +0330

    AddSeedData

---
+++ HR_Managment.Persistence/Configuration/Entities/LeaveRequestConfiguration.cs
@@ -0,0 +1,15 @@
+﻿
+using HR_Management.Domain.Entiteis;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace HR_Management.Persistence.Configurations.Entities
+{
+    public class LeaveRequestConfiguration : IEntityTypeConfiguration<LeaveRequest>
+    {
+        public void Configure(EntityTypeBuilder<LeaveRequest> builder)
+        {
+
+        }
+    }
+}
