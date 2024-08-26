2ab9a46b6fa0dab64834cd1692dc46a11fa7b95b
Ramin-Mohamadian <raminmohamadian2020@gmail.com>
2024-08-26 20:49:14 +0330

    Write Http Service

---
+++ HR_Managment.MVC/Services/Base/BaseHttpService.cs
@@ -0,0 +1,51 @@
+﻿using HR_Managment.MVC.Contracts;
+using System.Net.Http.Headers;
+
+namespace HR_Managment.MVC.Services.Base
+{
+    public class BaseHttpService
+    {
+        private readonly IClient _client;
+        private readonly IlocalStorageService _localStorageService;
+        public BaseHttpService(IClient client, IlocalStorageService localStorageService)
+        {
+            _client = client;
+            _localStorageService = localStorageService;
+        }
+
+        protected Response<Guid> ConvertApiException<Guid>(ApiException exception)
+        {
+            if (exception.StatusCode == 400)
+            {
+                return new Response<Guid>()
+                {
+                    Message = "Validation error have occourd.",
+                    ValidationError = exception.Response,
+                    Success = true
+                };
+
+            }
+            else if (exception.StatusCode == 404)
+            {
+                return new Response<Guid>()
+                {
+                    Message = "Not Found...",
+                    ValidationError = exception.Response,
+                    Success = false
+                };
+            }
+            return new Response<Guid>() { };
+        }
+
+
+
+        protected void AddBearerToken()
+        {
+            if (_localStorageService.Exists("Token"))
+            {
+                _client.HttpClient.DefaultRequestHeaders.Authorization =
+                    new AuthenticationHeaderValue("Bearer", _localStorageService.GetStorageValue<string>("Token"));
+            }
+        }
+    }
+}
